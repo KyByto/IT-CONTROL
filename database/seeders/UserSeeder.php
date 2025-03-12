1f225790fdc7285ae3e1bfb48aecc29695a05631
KyByto <haddadmohamedzakaria12@gmail.com>
2025-03-12 14:56:12 +0100

    admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+
+        User::firstOrCreate(
+            ['email' => 'admin@itcontrol.com'], // Condition de recherche
+            [
+
+                'name' => 'Admin',
+                'password' => bcrypt('itcontrol'),
+            ]
+        );
+
+
         User::factory(50)->create();
     }
 }
